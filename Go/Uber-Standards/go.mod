4b534320815d17f9bf54a55880a6079ad74a928b
Ranuga Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2026-01-06 18:29:02 +0530

    chore: some updates

---
+++ Go/Uber-Standards/go.mod
@@ -0,0 +1,3 @@
+module github.com/prdai/Learning/Go/Uber-Standards
+
+go 1.25.5
---
+++ Go/Uber-Standards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var x interface{}
+	x = 10
+	fmt.Printf("%s", x)
+	sync.Mutex
+}
